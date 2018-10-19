30a9d77b4ed2ccbe75a7bbedea39ec1152d1da87
Jose Luis Martinez <jlmartinez@capside.com>
2018-10-19 10:36:50 +0000

    Documentation

--- lib/Azure/AD/ClientCredentials.pm
+++ lib/Azure/AD/ClientCredentials.pm
@@ -133,3 +133,95 @@ package Azure::AD::ClientCredentials;
   }
 
 1;
+
+=encoding UTF-8
+
+=head1 NAME
+
+Azure::AD::ClientCredentials - Azure AD Client Credentials authentication flow
+
+=head1 SYNOPSIS
+
+  use Azure::AD::ClientCredentials;
+  my $creds = Azure::AD::ClientCredentials->new(
+    resource_id => 'https://management.core.windows.net/',
+    client_id => '',
+    secret_id => '',
+    tenant_id => '',
+  );
+  say $creds->access_token;
+
+=head1 DESCRIPTION
+
+Implements the Azure AD Client Credentials flow. See L<Azure::AD::Auth> for more
+information and alternative flows.
+
+=head1 ATTRIBUTES
+
+=head2 resource_id
+
+The URL for which you want a token extended (the URL of the service which you want
+to obtain a token for).
+
+C<https://graph.windows.net/> for using the MS Graph API
+
+C<https://management.core.windows.net/> for using the Azure Management APIs
+
+=head2 tenant_id
+
+The ID of the Azure Active Directory Tenant
+
+=head2 client_id
+
+The Client ID (also referred to as the Application ID) of an application
+
+=head2 secret_id
+
+A Key assigned to the Client Id.
+
+=head2 ad_url
+
+This defaults to C<https://login.microsoftonline.com>, and generally doesn't need to
+be specified. Azure AD has more endpoints for some clouds: 
+
+C<https://login.chinacloudapi.cn> China Cloud
+
+C<https://login.microsoftonline.us> US Gov Cloud
+
+C<https://login.microsoftonline.de> German Cloud
+
+=head1 METHODS
+
+=head2 access_token
+
+Returns the access token that has to be sent to the APIs you want to access. This
+is normally sent in the Authentication header of HTTPS requests as a Bearer token.
+
+The access_token is cached in the object as long as it's valid, so subsequent calls
+to access_token will return the appropiate token without reauthenticating to Azure AD. 
+If the token has expired, access_token will call Azure AD to obtain a new token transparently.
+
+Example usage:
+
+  my $auth = Azure::AD::ClientCredentials->new(...);
+
+  use HTTP::Tiny;
+  my $ua = HTTP::Tiny->new;
+  my $response = $ua->get(
+    'http://aservice.com/orders/list', 
+    {
+      headers => { Authorization => 'Bearer ' . $auth->access_token }
+    }
+  );
+
+=head1 SEE ALSO
+
+L<Azure::AD::Auth>
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2018 by CAPSiDE
+This code is distributed under the Apache 2 License. The full text of the 
+license can be found in the LICENSE file included with this module.
+
+=cut
--- lib/Azure/AD/DeviceLogin.pm
+++ lib/Azure/AD/DeviceLogin.pm
@@ -178,3 +178,104 @@ package Azure::AD::DeviceLogin;
   }
 
 1;
+
+=encoding UTF-8
+
+=head1 NAME
+
+Azure::AD::DeviceLogin - Azure AD Device Login authentication flow
+
+=head1 SYNOPSIS
+
+  use Azure::AD::DeviceLogin;
+  my $creds = Azure::AD::ClientCredentials->new(
+    resource_id => 'https://management.core.windows.net/',
+    message_handler => sub { say $_[0] },
+    client_id => '',
+    tenant_id => '',
+  );
+  say $creds->access_token;
+
+=head1 DESCRIPTION
+
+Implements the Azure AD Device Login flow. See L<Azure::AD::Auth> for more
+information and alternative flows.
+
+=head1 ATTRIBUTES
+
+=head2 resource_id
+
+The URL for which you want a token extended (the URL of the service which you want
+to obtain a token for).
+
+C<https://graph.windows.net/> for using the MS Graph API
+
+C<https://management.core.windows.net/> for using the Azure Management APIs
+
+=head2 message_handler
+
+Callback that receives the message for the user as it's first argument. This callback 
+should transmit the message to the end user, who has to follow the instructions embedded
+in it.
+
+=head2 tenant_id
+
+The ID of the Azure Active Directory Tenant
+
+=head2 client_id
+
+The Client ID (also referred to as the Application ID) of an application
+
+=head2 ad_url
+
+This defaults to C<https://login.microsoftonline.com>, and generally doesn't need to
+be specified. Azure AD has more endpoints for some clouds: 
+
+C<https://login.chinacloudapi.cn> China Cloud
+
+C<https://login.microsoftonline.us> US Gov Cloud
+
+C<https://login.microsoftonline.de> German Cloud
+
+=head1 METHODS
+
+=head2 access_token
+
+Returns the access token that has to be sent to the APIs you want to access. This
+is normally sent in the Authentication header of HTTPS requests as a Bearer token.
+
+The call to access_token will start the Device Login flow, which involves transmitting
+a message to the user (see message_handler attribute). The user will have to visit a 
+URL with a browser, insert the code in the message, authorize the application, and then 
+the authentication will proceed. Meanwhile the call to access_code will be blocked,
+awaiting the user to complete the flow. Once the user completes the instructions the access_code
+will be returned.
+
+The access_token is cached in the object as long as it's valid, so subsequent calls
+to access_token will return the appropiate token without reauthenticating to Azure AD. 
+If the token has expired, access_token will call Azure AD to obtain a new token.
+
+Example usage:
+
+  my $auth = Azure::AD::DeviceLogin->new(...);
+
+  use HTTP::Tiny;
+  my $ua = HTTP::Tiny->new;
+  my $response = $ua->get(
+    'http://aservice.com/orders/list', 
+    {
+      headers => { Authorization => 'Bearer ' . $auth->access_token }
+    }
+  );
+
+=head1 SEE ALSO
+
+L<Azure::AD::Auth>
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2018 by CAPSiDE
+This code is distributed under the Apache 2 License. The full text of the 
+license can be found in the LICENSE file included with this module.
+
+=cut
