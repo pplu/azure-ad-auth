89aa42bcc5bd975536292725c8c5be714332bb24
Jose Luis Martinez <jlmartinez@capside.com>
2018-11-23 16:19:38 +0000

    Bump auth provider module versions

--- lib/Azure/AD/Password.pm
+++ lib/Azure/AD/Password.pm
@@ -5,7 +5,7 @@ package Azure::AD::Password;
   use JSON::MaybeXS;
   use HTTP::Tiny;
 
-  our $VERSION = '0.01';
+  our $VERSION = '0.02';
 
   has ua_agent => (is => 'ro', isa => Str, default => sub {
     'Azure::AD::Password ' . $Azure::AD::Password::VERSION
